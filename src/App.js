87cb83c7a12c8bbf6fe33ef5c8eed4fda9381e32
Greg Pina <pinagm2825@gmail.com>
2019-02-10 20:50:14 -0500

    Set up basic lyric page
    Need to create Search and manipulate components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Provider } from './context';
+
 import './App.css';
 import Navbar from './components/layout/Navbar';
 
@@ -9,16 +11,18 @@ import Index from './components/layout/Index';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
